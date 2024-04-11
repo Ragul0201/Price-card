79ce83fa6b3351388df46d341b38dbf55a42a5cf
Ragul vinayak <ragulvinayakjan2@gmail.com>
2024-04-11 13:50:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react' //<React.Fragment></React.Fragment> --->another way to write a react fragment
+import Card from  './Component/Card'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let data = [{
+
+        plan:"FREE",
+        price : 0,
+        user : "Single user",
+        storage : "5GB Storage",
+        publicproject:"Unlimited Public Projects",
+        puPEnabler : true,
+        access: "Community Access",
+        accessEnabler : true,
+        privateproject:"Unlimited Private Projects",
+        prPEnabler : false,
+        support : "Dedicated Phone Support",
+        supportEnabler : false,
+        domain : "Free Subdomain",
+        domainEnabler : false,
+        report: "Monthly Status Reports",
+        reportEnabler  : false
+    }, {
+        plan:"PLUS",
+        price : 9,
+        user : "5 Users",
+        storage : "50GB Storage",
+        publicproject:"Unlimited Public Projects",
+        puPEnabler : true,
+        access: "Community Access",
+        accessEnabler : true,
+        privateproject:"Unlimited Private Projects",
+        prPEnabler : true,
+        support : "Dedicated Phone Support",
+        supportEnabler : true,
+        domain : "Free Subdomain",
+        domainEnabler : true,
+        report: "Monthly Status Reports",
+        reportEnabler  : false
+    }, {
+        plan:"PRO",
+        price : 49,
+        user : "Unlimited Users",
+        storage : "150GB Storage",
+        publicproject:"Unlimited Public Projects",
+        puPEnabler : true,
+        access: "Community Access",
+        accessEnabler : true,
+        privateproject:"Unlimited Private Projects",
+        prPEnabler : true,
+        support : "Dedicated Phone Support",
+        supportEnabler : true,
+        domain : "Free Subdomain",
+        domainEnabler : true,
+        report: "Monthly Status Reports",
+        reportEnabler  : true
+    }]
+    return <>
+        <section className="pricing py-5">
+            <div className="container">
+                <div className="row">
+                    {/* <Card/> props(property) contact one to another component */}
+                    {
+                        data.map((e,i)=>{
+                            return <Card detail={e} key={i}/>
+                        })
+                    }
+                </div>
+            </div>
+        </section>
+    </>
 }
 
-export default App;
+export default App
